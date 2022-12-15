0e0d9a6230800792dda18f7852bae10dabffe5a6
v <antyan2010@rambler.ru>
2022-12-15 13:05:43 +0300

    Example 008 HomeWork

---
+++ Example008_HomeWork/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+
+if (username == "AA")
+{
+    Console.WriteLine("Yaa, It is AAAA");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
+
